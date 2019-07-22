98393fd8e10f32571040e4cce61ae0911e346d6d
jyh7a <40563068+jyh7a@users.noreply.github.com>
2019-07-22 17:55:19 +0900

    바둑을위한 지뢰찾기 마지막? 공부
    https://www.youtube.com/watch?v=9sT4VoGn2Kw&list=PLcqDmjxt30Rtbxbh4eJREOVekql_kWVmu&index=50

---
+++ js/지뢰찾기.js
@@ -0,0 +1,8 @@
+
+document.querySelector('#exec').addEventListener('click', function(){
+  var hor = document.querySelector('#hor').value;
+  var ver = document.querySelector('#ver').value;
+  var mine = document.querySelector('#mine').value;
+
+  console.log(hor, ver, mine);
+});
